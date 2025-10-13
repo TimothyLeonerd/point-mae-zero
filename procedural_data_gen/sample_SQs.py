96164b160ccdbca9f98860348a7e2df136ec0d72
TimothyLeonerd <tim.leonerd@gmail.com>
2025-10-13 10:51:02 +0200

    changed output to np.array

--- procedural_data_gen/sample_SQs.py
+++ procedural_data_gen/sample_SQs.py
@@ -5,23 +5,29 @@ def sample_SQ_naive(sq_pars, n_theta, n_phi):
 
     a_x, a_y, a_z, eps_1, eps_2 = sq_pars
 
-    points = []
+    points = np.zeros((n_theta * n_phi, 3), dtype=float)
 
     d_theta = np.pi / n_theta
     d_phi = 2.0 * np.pi / n_phi
 
     theta = -np.pi/2.0 + np.pi/(2.0 * n_theta)
 
-    for i in range(0,n_theta):
+    idx = 0
+
+    for i in range(0, n_theta):
+
         phi = -np.pi + np.pi/n_phi
         for j in range(0, n_phi):
+
             x = a_x * np.cos(theta)**eps_1 * np.cos(phi)**eps_2
             y = a_y * np.cos(theta)**eps_1 * np.sin(phi)**eps_2
             z = a_z * np.sin(theta)**eps_1
 
-            points.append((x,y,z))
+            points[idx] = np.array([x,y,z], dtype=float)
             
             phi += d_phi
+            idx += 1
+            
         theta += d_theta
 
     return points
